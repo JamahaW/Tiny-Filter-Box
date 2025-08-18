a911b71c8baa3fd4fbef9499efffb9b32ed0a7b8
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-08-18 23:12:32 +0300

    fix

--- src/tfb/Median.hpp
+++ src/tfb/Median.hpp
@@ -5,32 +5,34 @@
 
 
 namespace tfb {
-    /// Медианный фильтр с буфером произвольного нечётного размера (2*N+1)
-    template<typename T, size_t N> struct MedianFilter {
-        static_assert(N % 2 == 1, "MedianFilter requires odd buffer size (2*K+1)");
 
-    private:
+/// Медианный фильтр с буфером произвольного нечётного размера (2*N+1)
+template<typename T, size_t N> struct MedianFilter {
+    static_assert(N % 2 == 1, "MedianFilter requires odd buffer size (2*K+1)");
 
-        std::array<T, N> buffer{};
-        size_t next_index{0};
+private:
 
-    public:
+    std::array<T, N> buffer{};
+    size_t next_index{0};
 
-        explicit MedianFilter(T init_value = 0) {
-            buffer.fill(init_value);
-        }
+public:
 
-        /// Рассчитать медиану
-        const T &calc(T &&value) {
-            buffer[next_index] = value;
-            next_index = (next_index + 1) % N;
+    explicit MedianFilter(T init_value = 0) {
+        buffer.fill(init_value);
+    }
 
-            auto sorted = buffer;
-            auto middle = sorted.begin() + N / 2;
+    /// Рассчитать медиану
+    const T &calc(const T &value) {
+        buffer[next_index] = value;
+        next_index = (next_index + 1) % N;
 
-            std::nth_element(sorted.begin(), middle, sorted.end());
+        auto sorted = buffer;
+        auto middle = sorted.begin() + N / 2;
 
-            return *middle;
-        }
-    };
+        std::nth_element(sorted.begin(), middle, sorted.end());
+
+        return *middle;
+    }
+};
+
 }
